c9b2062221b6bc83664e1d6fd250a43cd3c4b359
Marin Prusac <prusac.marin@gmail.com>
2025-05-24 20:10:20 +0200

    random spawns

--- Assets/Arena/RoundManager.cs
+++ Assets/Arena/RoundManager.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = System.Random;
 
 namespace Arena
 {
@@ -11,18 +13,33 @@ namespace Arena
         public static RoundManager Instance;
         
         public NetworkObject playerPrefab;
+        public Transform spawnsParent;
         
+        
+        
+        private readonly Dictionary<ulong, ClientObject> _clientObjects = new();
+        private readonly Dictionary<ulong, ArenaPlayer> _arenaPlayerObjects = new();
+        private readonly List<Vector3> _spawns = new();
 
-
-        private Dictionary<ulong, ClientObject> _clientObjects;
-        private Dictionary<ulong, ArenaPlayer> _arenaPlayerObjects;
+        private int _playerOneSpawnIndex;
+        private int _playerTwoSpawnIndex;
         
         public override void OnNetworkSpawn()
         {
             Instance = this;
 
-            _clientObjects = new Dictionary<ulong, ClientObject>();
-            _arenaPlayerObjects = new Dictionary<ulong, ArenaPlayer>();
+
+
+            
+            for (int i = 0; i < spawnsParent.childCount; i++)
+            {
+                _spawns.Add(spawnsParent.GetChild(i).position);
+            }
+
+            var rand = new Random();
+            _playerOneSpawnIndex = rand.Next(0, _spawns.Count-1);
+            _playerTwoSpawnIndex = (_playerOneSpawnIndex + rand.Next(1, _spawns.Count - 1)) % _spawns.Count;
+            
             foreach (var connectedClientsValue in NetworkManager.ConnectedClients.Values)
             {
                 _clientObjects[connectedClientsValue.ClientId] = connectedClientsValue.PlayerObject.GetComponent<ClientObject>();
@@ -39,7 +56,10 @@ namespace Arena
         [Rpc(SendTo.Server)]
         private void SpawnPlayerRpc(ulong clientId)
         {
-            var obj = NetworkManager.SpawnManager.InstantiateAndSpawn(playerPrefab, clientId, true, position: new Vector3(0, 1.337f, -36));
+
+            var spawn = _spawns[_arenaPlayerObjects.Count == 0 ? _playerOneSpawnIndex : _playerTwoSpawnIndex];
+            
+            var obj = NetworkManager.SpawnManager.InstantiateAndSpawn(playerPrefab, clientId, true, position: spawn);
             _arenaPlayerObjects[clientId] = obj.GetComponent<ArenaPlayer>();
         }
     }
